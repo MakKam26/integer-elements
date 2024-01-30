11dcacf6d05e0dbe21f25c7f7d223262f38aa169
MakKam26 <kamzra@googlemail.com>
2024-01-30 21:37:37 +0000

    first commit

---
+++ integerElements.java
@@ -0,0 +1,27 @@
+public class integerElements{
+    public static void main(String[] args){
+
+        int[] arrayIntExample = new int[8];
+
+            arrayIntExample[0] = 2;
+            arrayIntExample[1] = 4;
+            arrayIntExample[2] = 6;
+            arrayIntExample[3] = 8;
+            arrayIntExample[4] = 10;
+            arrayIntExample[5] = 12;
+            arrayIntExample[6] = 14;
+            arrayIntExample[7] = 16;
+            
+        System.out.println(arrayIntExample [4]);
+
+
+
+
+
+    }
+
+
+
+
+
+}
